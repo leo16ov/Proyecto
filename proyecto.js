a7af8855ce2538e18c3b82c21c3d3432a6f0bee2
Leo Ovando <leoxpro050@gmail.com>
2022-11-09 20:11:11 -0300

    agregandole funcionalidad onclick al encabezado del menu

--- proyecto.js
+++ proyecto.js
@@ -1,6 +1,9 @@
-function modoscuro (){
+function saludar() {
+    alert("Bienvenido a Creaciones Mily, Somos un emprendiemiento que se dedica a vender ropa de mujer y de hombre al Por Mayor y Por Menor.")
+    }
+
+function modoscuro() {
 
     var elemento = document.body;
     elemento.classList.toggle("oscurecer");
-
 }
